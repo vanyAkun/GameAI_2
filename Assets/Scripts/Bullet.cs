3d9034eaf4d31a9d1c8eb0c04dcbc3f7079538bc
Vanesa Mendez <73304237+vanyAkun@users.noreply.github.com>
2023-11-19 22:45:42 +0000

    NPC Chase+Bullet fire

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float Speed = 10f;
+    public float Speed = 15f;
+    Rigidbody rigidBody;
 
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+
+    }
+    public void InitializeBullet(Vector3 originalDirection)
+    {
+        print(originalDirection);
+        transform.forward = originalDirection;
+        rigidBody.velocity = transform.forward * Speed;
+    }
     private void OnCollisionEnter(Collision collision)
     {
         Destroy(gameObject);
--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -35,7 +35,11 @@ public class NPC : MonoBehaviour
     NPCStates currentState = NPCStates.Patrol;
     NavMeshAgent navMeshAgent;
     MeshRenderer meshRenderer;
+
     float nextShootTime = 0;
+
+    public Transform bulletPosition;
+    public GameObject bulletPrefab;
 
     void Start()
     {
@@ -48,7 +52,17 @@ public class NPC : MonoBehaviour
     {
         SwitchState();
     }
+    void Fire()
+    {
+        if (Time.time > nextShootTime)
+        {
+            nextShootTime = Time.time + FireRate;
 
+            GameObject bullet = Instantiate(bulletPrefab, bulletPosition.position, Quaternion.identity);
+            bullet.GetComponent<Bullet>()?.InitializeBullet(transform.rotation * Vector3.forward);
+
+        }
+    }
     private void SwitchState()
     {
         switch (currentState)
@@ -70,16 +84,56 @@ public class NPC : MonoBehaviour
 
     private void Attack()
     {
-        // Your code
+        navMeshAgent.ResetPath(); // Reset the NPC's path
+        navMeshAgent.isStopped = true; // Explicitly stop the NPC's movement
+        meshRenderer.material = AttackMaterial; // Change material to attack material
+        transform.LookAt(Player); // Ensure the NPC looks at the player
+
+        Fire(); // Attempt to fire a bullet
+
+        // Check if the player is out of attack range
+        if (Vector3.Distance(transform.position, Player.position) > AttackRange)
+        {
+            navMeshAgent.isStopped = false; // Allow the NPC to move again
+            currentState = NPCStates.Chase;
+        }
     }
 
     private void Chase()
     {
-        // Your code
+        meshRenderer.material = ChaseMaterial; // Change material to chase material
+        navMeshAgent.SetDestination(Player.position); // Set destination to player's position
+
+        // Check if player is within attack range
+        if (Vector3.Distance(transform.position, Player.position) <= AttackRange)
+        {
+            currentState = NPCStates.Attack;
+        }
+        // Check if player is out of chase range
+        else if (Vector3.Distance(transform.position, Player.position) > ChaseRange)
+        {
+            navMeshAgent.ResetPath(); // Reset the path
+            currentState = NPCStates.Patrol;
+        }
     }
 
     private void Patrol()
     {
-        // Your code
+        if (Vector3.Distance(transform.position, Player.position) < ChaseRange)
+        {
+            currentState = NPCStates.Chase;
+            return; // Exit the method to avoid executing remaining code when state changes
+        }
+
+        // If the NPC is not moving
+        if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance < 0.5f)
+        {
+            meshRenderer.material = PatrolMaterial; // Change material to patrol material
+
+            // Increment patrol point and reset if needed
+            nextPatrolPoint = (nextPatrolPoint + 1) % PatrolPoints.Length;
+            navMeshAgent.SetDestination(PatrolPoints[nextPatrolPoint]);
+        }
     }
 }
+
