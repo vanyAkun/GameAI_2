4357e8912d454992bcf2e80f5b0b1187145afc5b
Vanesa Mendez <73304237+vanyAkun@users.noreply.github.com>
2023-11-23 15:31:42 +0000

    Seeker finds item via pathfinding

--- Assets/Scripts/Pathfinding Hell/Node.cs
+++ Assets/Scripts/Pathfinding Hell/Node.cs
@@ -1,57 +1,45 @@
+﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class Node : IHeapItem<Node>
-{
-    public bool walkable;
-    public Vector3 worldPosition;
-    public int gridX;
-    public int gridY;
-    public int movementPenalty;
+public class Node : IHeapItem<Node> {
+	
+	public bool walkable;
+	public Vector3 worldPosition;
+	public int gridX;
+	public int gridY;
 
-    public int gCost;
-    public int hCost;
-    public Node parent;
-    int heapIndex;
+	public int gCost;
+	public int hCost;
+	public Node parent;
+	int heapIndex;
+	
+	public Node(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY) {
+		walkable = _walkable;
+		worldPosition = _worldPos;
+		gridX = _gridX;
+		gridY = _gridY;
+	}
 
+	public int fCost {
+		get {
+			return gCost + hCost;
+		}
+	}
 
-    public Node(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY, int _penalty)
-    {
-        walkable = _walkable; worldPosition = _worldPos;
-        this.gridX = _gridX;
-        this.gridY = _gridY;
-        movementPenalty = _penalty;
-    }
+	public int HeapIndex {
+		get {
+			return heapIndex;
+		}
+		set {
+			heapIndex = value;
+		}
+	}
 
-    public int fCost
-    {
-        get
-        {
-            return gCost + hCost;
-        }
-    }
-
-    public int HeapIndex
-    {
-        get
-        {
-            return heapIndex;
-        }
-        set
-        {
-            HeapIndex = value;
-        }
-
-    }
-
-    public int CompareTo(Node nodeToCompare)
-    {
-        int compare = fCost.CompareTo(nodeToCompare.fCost);
-        if (compare == 0)
-        {
-            compare = hCost.CompareTo(nodeToCompare.hCost);
-        }
-        return -compare;
-    }
+	public int CompareTo(Node nodeToCompare) {
+		int compare = fCost.CompareTo(nodeToCompare.fCost);
+		if (compare == 0) {
+			compare = hCost.CompareTo(nodeToCompare.hCost);
+		}
+		return -compare;
+	}
 }
--- Assets/Scripts/Pathfinding Hell/PathRequestManager.cs
+++ Assets/Scripts/Pathfinding Hell/PathRequestManager.cs
@@ -1,60 +1,53 @@
+﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System;
-using System.Data;
 
-public class PathRequestManager : MonoBehaviour
-{
-    Queue<PathRequest> pathRequestQueue = new Queue<PathRequest>();
-    PathRequest currentPathRequest;
+public class PathRequestManager : MonoBehaviour {
 
-    static PathRequestManager instance;
-    Pathfinding pathfinding;
+	Queue<PathRequest> pathRequestQueue = new Queue<PathRequest>();
+	PathRequest currentPathRequest;
 
-    bool isProcessingpath;
+	static PathRequestManager instance;
+	Pathfinding pathfinding;
 
-    private void Awake()
-    {
-        instance = this;
-        pathfinding = GetComponent<Pathfinding>();
-    }
+	bool isProcessingPath;
 
-    public static void RequestPath(Vector3 pathStart, Vector3 pathEnd, Action<Vector3[], bool> callback)
-    {
-        PathRequest newRequest = new PathRequest(pathStart, pathEnd, callback);
-        instance.pathRequestQueue.Enqueue(newRequest);
-        instance.TryProcessNext();
-    }
-    void TryProcessNext()
-    {
-        if (!isProcessingpath && pathRequestQueue.Count > 0)
-        {
-            currentPathRequest = pathRequestQueue.Dequeue();
-            isProcessingpath = true;
-            pathfinding.StartFindPath(currentPathRequest.pathStart, currentPathRequest.pathEnd);
-        }
-    }
-    public void FinishedProcessingPath(Vector3[] path, bool success)
-    {
-        currentPathRequest.callback(path, success);
-        isProcessingpath = false;
-        TryProcessNext(); 
-        
-    }
-}
+	void Awake() {
+		instance = this;
+		pathfinding = GetComponent<Pathfinding>();
+	}
 
+	public static void RequestPath(Vector3 pathStart, Vector3 pathEnd, Action<Vector3[], bool> callback) {
+		PathRequest newRequest = new PathRequest(pathStart,pathEnd,callback);
+		instance.pathRequestQueue.Enqueue(newRequest);
+		instance.TryProcessNext();
+	}
 
-struct PathRequest 
-{
-    public Vector3 pathStart;
-    public Vector3 pathEnd;
-    public Action<Vector3[], bool> callback;
+	void TryProcessNext() {
+		if (!isProcessingPath && pathRequestQueue.Count > 0) {
+			currentPathRequest = pathRequestQueue.Dequeue();
+			isProcessingPath = true;
+			pathfinding.StartFindPath(currentPathRequest.pathStart, currentPathRequest.pathEnd);
+		}
+	}
 
-    public PathRequest(Vector3 _start, Vector3 _end, Action<Vector3[], bool> _callback)
-    {
-        pathStart = _start;
-        pathEnd = _end;
-        callback = _callback;
-    }
+	public void FinishedProcessingPath(Vector3[] path, bool success) {
+		currentPathRequest.callback(path,success);
+		isProcessingPath = false;
+		TryProcessNext();
+	}
+
+	struct PathRequest {
+		public Vector3 pathStart;
+		public Vector3 pathEnd;
+		public Action<Vector3[], bool> callback;
+
+		public PathRequest(Vector3 _start, Vector3 _end, Action<Vector3[], bool> _callback) {
+			pathStart = _start;
+			pathEnd = _end;
+			callback = _callback;
+		}
+
+	}
 }
