ee9aeca1db58235194447fcbd07db36e3ed82d42
Vanesa Mendez <73304237+vanyAkun@users.noreply.github.com>
2023-11-23 11:45:13 +0000

    Added obstacles and pathfinding

---
+++ Assets/Scripts/Pathfinding Hell/Node.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : IHeapItem<Node>
+{
+    public bool walkable;
+    public Vector3 worldPosition;
+    public int gridX;
+    public int gridY;
+    public int gCost;
+    public int hCost;
+    public Node parent;
+    int heapIndex;
+
+
+    public Node(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY)
+    {
+        walkable = _walkable; worldPosition = _worldPos;
+        this.gridX = _gridX;
+        this.gridY = _gridY;
+    }
+
+    public int fCost
+    {
+        get
+        {
+            return gCost + hCost;
+        }
+    }
+
+    public int HeapIndex
+    {
+        get
+        {
+            return heapIndex;
+        }
+        set
+        {
+            HeapIndex = value;
+        }
+
+    }
+
+    public int CompareTo(Node nodeToCompare)
+    {
+        int compare = fCost.CompareTo(nodeToCompare.fCost);
+        if (compare == 0)
+        {
+            compare = hCost.CompareTo(nodeToCompare.hCost);
+        }
+        return -compare;
+    }
+}
---
+++ Assets/Scripts/Pathfinding Hell/Pathfinding.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+public class Pathfinding : MonoBehaviour
+{
+    public Transform seeker, target;
+    Grid grid;
+
+    private void Awake()
+    {
+        {
+            grid = GetComponent<Grid>();
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            FindPath(seeker.position, target.position);
+        }
+    }
+    void FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        Node startNode=grid.NodeFromWorldPoint(startPos);
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        List<Node> openSet = new List<Node>();
+        HashSet<Node>closedSet = new HashSet<Node>();
+        openSet.Add(startNode);
+
+        while(openSet.Count > 0)//THIS IS THE SLOWEST, SO WE NEED TO OPTIMISE IT BABYYYYY
+        {
+            Node currentNode = openSet[0];
+            for(int i=1;i<openSet.Count;i++)
+            {
+                if (openSet[i].fCost < currentNode.fCost|| openSet[i].fCost ==currentNode.fCost && openSet[i].hCost < currentNode.hCost)
+                {
+                    currentNode = openSet[i];
+                }
+                
+            }
+            openSet.Remove(currentNode);
+            closedSet.Add(currentNode);
+
+            if (currentNode == targetNode)
+            {sw.Stop();
+                print("Path found: " + sw.ElapsedMilliseconds + "ms");
+                RetracePath(startNode, targetNode);
+                return;
+            }
+            foreach (Node neighbour in grid.GetNeighbours(currentNode))
+            {
+                if ((!neighbour.walkable) || closedSet.Contains(neighbour))
+                {
+                    continue;
+                }
+                int newMovementCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbour);
+                if (newMovementCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour))
+                {
+                    neighbour.gCost = newMovementCostToNeighbour;
+                    neighbour.hCost = GetDistance(neighbour, targetNode);
+                    neighbour.parent = currentNode;
+
+                    if (!openSet.Contains(neighbour))   
+                        openSet.Add(neighbour);
+                }
+            }
+        }
+    }
+    void RetracePath(Node startNode, Node endNode)
+    {
+        List<Node> path = new List<Node>();
+        Node currentNodde = endNode;
+
+        while(currentNodde != startNode)
+        {
+            path.Add(currentNodde);
+            currentNodde=currentNodde.parent;
+        }
+        path.Reverse();
+
+        grid.path = path;
+    }
+    int GetDistance (Node nodeA, Node nodeB)
+    {
+        int dstX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+        int dstY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+        if (dstX > dstY)
+            return 14*dstY ^+10*(dstX-dstY);
+        return 14* dstX+10 *(dstY-dstY);    
+    }
+}
+
